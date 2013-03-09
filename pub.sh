f9e02c15755d1d39dd655c53101e0b1301f538d5
Richard Vaughan <vaughan@sfu.ca>
2013-03-08 18:16:44 -0800

    fixing pub bugs

--- pub.sh
+++ pub.sh
@@ -2,6 +2,10 @@
 
 export TMPDIR=.
 
+bibtool autolab.bib all.bib
+bibtex2html -r -d -revkeys -nodoc all.bib 
+
+
 for T in chapter inproceedings thesis article techreport mastersthesis phdthesis incollection misc; do
   bibtool -- select{@$T} autolab.bib -o $T.bib
   bibtex2html -r -d -revkeys -nodoc $T.bib 
